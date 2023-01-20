c588b52a83b5c46a2041b9b9820efd292e8cf04c
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-01-19 21:41:05 -0400

    feat:add filter by name of search

---
+++ src/components/Search/booksDatabase.js
@@ -0,0 +1,11 @@
+import book from "../../download-imagens/livro.png";
+import book2 from "../../download-imagens/livro2.png";
+
+const booksDatabase = [
+  { nome: "Liderança em Design", id: 1, src: book },
+  { nome: "JavaScript Assertivo", id: 2, src: book2 },
+  { nome: "Cangaceiro em JavaScript", id: 3, src: book },
+  { nome: "Aventureiros do Java", id: 4, src: book2 },
+];
+
+export default booksDatabase;
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,13 +2,28 @@ import { Input } from "../Input/styles";
 import { SearchContainer } from "./styles";
 import { Title } from "./styles";
 import { Subtitle } from "./styles";
+import { useState } from "react";
+import booksDatabase from "./booksDatabase";
 
 function Search() {
+  const [inputSearch, setInputSearch] = useState([]);
+  console.log(inputSearch);
+
   return (
     <SearchContainer>
       <Title>Já sabe por onde começar?</Title>
       <Subtitle>Encontre seu livro em nossa estante</Subtitle>;
-      <Input placeholder="Escreva sua proxima leitura" />
+      <Input
+        placeholder="Escreva sua proxima leitura"
+        onBlur={(event) => {
+          const text = event.target.value.trim().toLowerCase();
+
+          const filteredBook = booksDatabase.filter((book) =>
+            book.nome.toLowerCase().includes(text)
+          );
+          setInputSearch(filteredBook);
+        }}
+      />
     </SearchContainer>
   );
 }
